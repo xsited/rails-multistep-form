54508caff6263e70ffa01d33eb087163fb1d5e72
Thomas Kee <t.kee@temperednetworks.com>
2018-07-25 11:06:03 -0700

    helper templates

---
+++ app/helpers/templates_helper.rb
@@ -0,0 +1,9 @@
+module TemplatesHelper
+  def build_template(template)
+  	# XXX - enable!!
+    # template.hipswitches.build if template.hipswitches.empty?
+    # template.deployments.build if template.deployments.empty?
+    # template.hosts.build if template.hosts.empty?
+    template
+  end
+end
